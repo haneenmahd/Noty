c54959da0b79312e2e2ede15d601a02a43c339f8
Haneen Mahdin <haneenmahdin@gmail.com>
2022-04-20 19:22:21 +0530

    Initial Commit

---
+++ Noty/NotyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotyApp.swift
+//  Noty
+//
+//  Created by Haneen Mahdin on 20/04/22.
+//
+
+import SwiftUI
+
+@main
+struct NotyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
